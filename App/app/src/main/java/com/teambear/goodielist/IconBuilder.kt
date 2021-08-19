7598d6016bdefe0bb9398f20adf1d0dff5e601b5
Muzuwi <kedziora99@gmail.com>
2021-08-19 13:11:02 +0200

    Improve icon builder

--- App/app/src/main/java/com/teambear/goodielist/IconBuilder.kt
+++ App/app/src/main/java/com/teambear/goodielist/IconBuilder.kt
@@ -1,34 +1,41 @@
 package com.teambear.goodielist
 
 object IconBuilder {
-    val iconHashMap = hashMapOf<String, Int>(
-        "unknown"   to R.drawable.icon_unknown,
-        "multi"     to R.drawable.icon_multi,
-        "fish"      to R.drawable.icon_fish,
-        "meat"      to R.drawable.icon_meat,
-        "salad"     to R.drawable.icon_salad,
-        "soup"      to R.drawable.icon_soup,
-        "soup2"     to R.drawable.icon_soup2,
-        "drink"     to R.drawable.icon_drink,
-        "dessert"   to R.drawable.icon_dessert,
+    private const val unknownIcon = R.drawable.icon_unknown
+    private const val multipleIcon = R.drawable.icon_multi
+
+    val iconHashMap = hashMapOf(
+        "breakfast"     to R.drawable.icon_breakfest,
+        "dessert"       to R.drawable.icon_dessert,
+        "drink"         to R.drawable.icon_drink,
+        "fish"          to R.drawable.icon_fish,
+        "lunch"         to R.drawable.icon_lunch,
+        "dinner"        to R.drawable.icon_lunch,
+        "meat"          to R.drawable.icon_meat,
+        "salad"         to R.drawable.icon_salad,
+        "vegetables"    to R.drawable.icon_salad,
+        "soup"          to R.drawable.icon_soup,
+        "supper"        to R.drawable.icon_supper
     )
 
     fun getIconId(tagList: List<String>): Int{
-        val relatedTags = mutableListOf<String>()
+        val tagIcons = mutableListOf<Int>()
         for (tag in tagList) {
-            if(iconHashMap.containsKey(tag)) relatedTags.add(tag);
+            if(iconHashMap.containsKey(tag.lowercase())) {
+                tagIcons.add(iconHashMap[tag.lowercase()]!!)
+            }
         }
 
-        when(relatedTags.size){
-            0 -> return iconHashMap["unknown"]!!
-            1 -> return iconHashMap[relatedTags[0]]!!
-            else -> return iconHashMap["multi"]!!
+        return when(tagIcons.size){
+            0       -> unknownIcon
+            1       -> tagIcons.first()
+            else    -> multipleIcon
         }
     }
 
     fun getIconId(tag: String): Int{
-        if(iconHashMap.containsKey(tag))
-            return iconHashMap[tag]!!
-        return iconHashMap["unknown"]!!
+        if(iconHashMap.containsKey(tag.lowercase()))
+            return iconHashMap[tag.lowercase()]!!
+        return unknownIcon
     }
 }
